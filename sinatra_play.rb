59ca0f89e6e81af54123ddc41b1613afbbeb1887
Chau Hong Linh <chauhonglinh@gmail.com>
2015-06-11 02:16:12 -0700

    Change the way sinatra_play requires files.

--- sinatra_play.rb
+++ sinatra_play.rb
@@ -6,7 +6,7 @@ require_relative 'config/html_statuses'
 
 Dir.entries('.').select { |x| File.directory?(x)  && x != '.' && x != '..'  && x != 'spec' }.each  do |sub_path|
   rb_files =  Dir.glob("#{File.expand_path('.')}/#{sub_path}/*.rb")
-  rb_files.each { |rb_file| require rb_file unless rb_file =~ /deploy/ || rb_file =~ /puma.rb/ }
+  rb_files.each { |rb_file| require rb_file unless rb_file =~ /deploy.rb/ || rb_file =~ /puma.rb/ }
 end
 
 class SinatraPlay < Sinatra::Base
